76f96e6b4df8afee4a11834c77d78d4330f02b1f
zaveri10 <daniel.zaveri@gmail.com>
2025-03-25 18:58:50 -0300

    up

--- plataforma/application/views/header.php
+++ plataforma/application/views/header.php
@@ -1,18 +1,17 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="MNESE">
+	<meta name="description" content="HeadAI">
 
 	<title><?php print($titulo); ?></title>
 
-	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap/bootstrap.min.css')?>">
-	
-	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap/bootstrap.min.css') ?>">
+
+	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css') ?>">
 
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-  
+
 
 </head>
-
